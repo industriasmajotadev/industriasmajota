c0c075293b5e2da8be215fa6defeb3f9acbcb6ef
industriasmajotadev <160252745+industriasmajotadev@users.noreply.github.com>
2024-02-16 11:21:15 -0400

    Inital commit

---
+++ procesa.js
@@ -0,0 +1,107 @@
+//FUNCIONESDELOS BOTONES  QUE SE EJECUTA PARA DIRIGIR ALAS REDES SOCIALES 
+
+function direccionfacebook() {
+    window.location.href = "https://www.facebook.com/industriasmajota/?locale=es_LA";
+}
+function direccionwatsap() {
+    window.location.href = "https://wa.link/3165ps";
+}
+
+function direcciontiktok() {
+    window.location.href = "https://www.tiktok.com/@industriasmajota/video/7178696394868296965";
+}
+
+function direccionyoutube() {
+    window.location.href = "https://www.youtube.com/@VictorTaborgaMontecinos";
+}
+
+
+function direccionafotos() {
+    window.location.href = "file:///C:/Users/usuario/Desktop/PAGINA-WEB/galeria.html";
+}
+
+
+
+
+
+
+//FUNCIONES DELAS MAQUINAS INDUSTRIALES  Y EL OVERLAY CUNADO MUESTRA EN LA MINI VENTANA  
+
+function showProductDetails() {
+    document.getElementById('modal').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block'; // Mostrar el overlay
+}
+
+// Función para cerrar el modal
+function closeModal() {
+    document.getElementById('modal').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none'; // Ocultar el overlay
+}
+
+
+function abrir() {
+    document.getElementById('modal1').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block'; // Mostrar el overlay
+}
+
+// Función para cerrar el modal
+function cerrar() {
+    document.getElementById('modal1').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none'; // Ocultar el overlay
+}
+
+// Función para mostrar el modal
+function abierto() {
+    document.getElementById('modal3').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block'; // Mostrar el overlay
+}
+
+// Función para cerrar el modal
+function serrado() {
+    document.getElementById('modal3').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none'; // Ocultar el overlay
+}
+
+
+// Función para mostrar el modal
+function descubre() {
+    document.getElementById('modal5').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block'; // Mostrar el overlay
+}
+
+// Función para cerrar el modal
+function cubierto() {
+    document.getElementById('modal5').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none'; // Ocultar el overlay
+}
+
+
+
+// Función para mostrar el modal
+function blanco() {
+    document.getElementById('modal6').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block'; // Mostrar el overlay
+}
+
+// Función para cerrar el modal
+function negro() {
+    document.getElementById('modal6').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none'; // Ocultar el overlay
+}
+
+
+
+// Función para mostrar el modal
+function arriba() {
+    document.getElementById('modal2').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block'; // Mostrar el overlay
+}
+
+// Función para cerrar el modal
+function abajo() {
+    document.getElementById('modal2').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none'; // Ocultar el overlay
+}
+
+
+
